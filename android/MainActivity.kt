1bcb42282154932dd3f5ed88498582f77090a988
sidss01 <144091560+sidss01@users.noreply.github.com>
2026-02-12 20:09:35 +0530

    Create MainActivity.kt for Android karaoke application

---
+++ android/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.karaokeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Initialize karaoke features and UI components here
+    }
+}
